26808013b592eecbc312fc7d409d06457720c772
Yugal Khanter <yugalkhanter82@gmail.com>
2023-02-02 16:02:50 +0530

    added login route and functionality, also added jwt tokens

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 
-const user = mongoose.Schema({
+const userSchema = mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Name is a mandatory field'],
@@ -21,6 +21,7 @@ const user = mongoose.Schema({
         type: String,
         required: [true, 'Password is a mandatory field'],
         minLength: 6,
+        select: false,
     },
     confirmPassword: {
         type: String,
@@ -28,24 +29,34 @@ const user = mongoose.Schema({
         minLength: 6,
         validate: {
             // This only works on CREATE and SAVE method
-            validator: function(el){
+            validator: function (el) {
                 return el === this.password;
             },
             message: 'Password and Confirm Password are not matching.'
-        }
+        },
+        select: false
     }
 });
 
-user.pre('save', async function(next){
+// this middleware is to hash the password using bcryptjs package
+// Note that this can be done in controller file also, but since we follow fat model and thin controller concept, we put all the data related operations in models file.
+userSchema.pre('save', async function (next) {
     // only run this function only when password is modified
-    if(!this.isModified('password')) return next();
+    if (!this.isModified('password')) return next();
 
     // hash the password with salt 12.
     this.password = await bcrypt.hash(this.password, 12);
     this.confirmPassword = undefined;
     next();
-})
+});
 
-const Users = mongoose.model('users', user);
+userSchema.methods.correctPassword = async function (
+    candidatePassword,
+    userPassword
+) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const Users = mongoose.model('users', userSchema);
 
 module.exports = Users;
