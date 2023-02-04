cf7b8e424150418c62d2a0a2cfc6e73ea2cfefc4
Yugal Khanter <yugalkhanter82@gmail.com>
2023-02-05 01:24:41 +0530

    added protection to tour routes i.e verifying user if logged in or not

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ app.get('/', (req, res) => {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    // console.log(req.requestTime)
+    
+    // console.log(req.headers);
+    next();
+})
+
 // 2) Routes
 app.use(tourRoutes);
 app.use(usersRoutes);
--- routes/tours.js
+++ routes/tours.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const { getAllTours, getTourByID, postTours, updateTourByID, deleteTourByID, getTourStats } = require('../controllers/toursController');
+const { protect } = require('../controllers/authController');
 
-router.get('/api/v1/tours', getAllTours);
+router.get('/api/v1/tours', protect, getAllTours);
 router.get('/api/v1/tours/tour-stats', getTourStats);
 router.get('/api/v1/tours/:id', getTourByID);
 router.post('/api/v1/tours', postTours);
