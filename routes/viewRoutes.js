713419132bccd5d5a81cf84dd4dddd5afdcb894f
Yugal Khanter <yugalkhanter82@gmail.com>
2023-02-26 16:37:40 +0530

    added 404 page, my-profile(accounts) page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllTours, getTourBySlug, loginForm } = require('../controllers/viewsController');
-const { isLoggedIn } = require('../controllers/authController');
+const { getAllTours, getTourBySlug, loginForm, getAccount } = require('../controllers/viewsController');
+const { isLoggedIn, protect } = require('../controllers/authController');
     
 
 router.get('/', isLoggedIn, getAllTours);
 router.get('/tour/:slug', isLoggedIn, getTourBySlug);
 router.get('/login', isLoggedIn, loginForm);
+router.get('/my-profile', protect, getAccount);
 
 module.exports = router;
