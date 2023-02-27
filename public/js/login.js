c84dae0e1952a1faa389a5589bb1db45dae19b99
Yugal Khanter <yugalkhanter82@gmail.com>
2023-02-27 18:28:34 +0530

    update user data and user password functionality added

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,5 @@
 
+// to login the user
 const login = async (email, password) => {
     try {
         const res = await axios({
@@ -22,7 +23,7 @@ const login = async (email, password) => {
     }
 }
 
-const form = document.querySelector('.form');
+const form = document.querySelector('.form--login');
 if (form) {
     form.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -33,6 +34,7 @@ if (form) {
     });
 }
 
+// to logout the user
 const logout = async () => {
     try {
         const res = await axios({
@@ -51,4 +53,68 @@ const logout = async () => {
 const logoutBtn = document.querySelector('.nav__el--logout');
 if (logoutBtn) {
     logoutBtn.addEventListener('click', logout);
+}
+
+
+// to update the user's data
+const updateUserData = async (name, email) => {
+    try {
+        const body = {
+            name,
+            email
+        }
+        const res = await axios.patch('http://localhost:8000/api/v1/users/update', body);
+
+        if (res.data.status == 'Success') {
+            alert('Data updated successfully!');
+        }
+    } catch (error) {
+        alert('Error while updating your data. Try again!', error.message);
+    }
+}
+
+const updateUserForm = document.querySelector('.form-user-data');
+if (updateUserForm) {
+    updateUserForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+
+        updateUserData(name, email);
+    });
+}
+
+// to update the user's password
+const updatePassword = async (passwordCurrent, password, confirmPassword) => {
+    try {
+        const body = {
+            passwordCurrent,
+            password,
+            confirmPassword
+        }
+        const res = await axios.patch('http://localhost:8000/update-password', body);
+        if (res.data.status == 'Success') {
+            alert('Password updated successfully!');
+        }
+    } catch (error) {
+        alert(error.response.data.message);
+    }
+}
+
+const updatePasswordForm = document.querySelector('.form-user-password');
+if (updatePasswordForm) {
+    updatePasswordForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        const passwordCurrent = document.getElementById('password-current').value;
+        const password = document.getElementById('password').value;
+        const confirmPassword = document.getElementById('password-confirm').value;
+
+        await updatePassword(passwordCurrent, password, confirmPassword);
+
+        document.getElementById('password-current').value = ''
+        document.getElementById('password').value = ''
+        document.getElementById('password-confirm').value = ''
+    })
 }
